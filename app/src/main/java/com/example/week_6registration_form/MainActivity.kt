f9a933afdcbd8cf9650e40908945b7ec7509e49d
Ransom Ahanmisi <60149483+Ransom8@users.noreply.github.com>
2020-08-26 02:23:43 +0100

    First Backend

--- app/src/main/java/com/example/week_6registration_form/MainActivity.kt
+++ app/src/main/java/com/example/week_6registration_form/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.example.week_6registration_form
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nameText: EditText
+    lateinit var emailText: EditText
+    lateinit var phoneNumber: EditText
+    lateinit var spinner: Spinner
+    lateinit var name: String
+    lateinit var email: String
+    lateinit var number: String
+    lateinit var sex: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        nameText = findViewById(R.id.editTextPersonName)
+        emailText = findViewById(R.id.editTextEmailAddress)
+        phoneNumber = findViewById(R.id.editTextPhone)
+        spinner = findViewById(R.id.spinner)
+
         button.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
+            name = nameText.text.toString()
+            email = emailText.text.toString()
+            number = phoneNumber.text.toString()
+            sex = spinner.selectedItem.toString()
+            val intent = Intent(this, SecondActivity::class.java).also {
+                it.putExtra("name", name)
+                it.putExtra("email", email)
+                it.putExtra("number", number)
+                it.putExtra("sex", sex)
+            }
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/week_6registration_form/SecondActivity.kt
+++ app/src/main/java/com/example/week_6registration_form/SecondActivity.kt
@@ -1,11 +1,34 @@
 package com.example.week_6registration_form
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var nameOut: TextView
+    lateinit var telOut: TextView
+    lateinit var emailOut: TextView
+    lateinit var sexOut: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val name = intent.getStringExtra("name")
+        val number = intent.getStringExtra("number")
+        val email = intent.getStringExtra("email")
+        val sex = intent.getStringExtra("sex")
+
+        nameOut = findViewById(R.id.editTextTextPersonName3)
+        telOut = findViewById(R.id.editTextPhone2)
+        emailOut = findViewById(R.id.editTextTextEmailAddress2)
+        sexOut = findViewById(R.id.genderView)
+
+        nameOut.text = name
+        telOut.text = number
+        emailOut.text = email
+        sexOut.text = sex
     }
 }
